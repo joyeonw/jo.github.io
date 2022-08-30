c5df8cca2446e348f3f3260b5479c50522e426a5
joyeonw <106740152+joyeonw@users.noreply.github.com>
2022-08-30 21:50:17 +0900

    Add files via upload

---
+++ html/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
+    });
+
+});//jquery end
